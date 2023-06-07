b4728dda904d5d3e431add3d6ac34a74b0d32fe2
eliyon <55352187+Eliyonqwe@users.noreply.github.com>
2023-06-07 09:50:39 +0300

    experience, machine sold, employees added

--- plastic.js
+++ plastic.js
@@ -6,4 +6,25 @@ function openNav() {
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
-  } 
+  }
+
+
+
+
+  let valueDisplays = document.querySelectorAll(".nums");
+  let interval = 1000;
+  valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    console.log(endValue);
+
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function(){
+      startValue+=1;
+      valueDisplay.textContent = startValue;
+      if(startValue == endValue){
+        clearInterval(counter);
+      }
+    }, duration);
+  });
+  
